c3d27eed704416a417264e852a605899dd75f8a3
tragedy <79763373+trxgedy@users.noreply.github.com>
2022-10-26 11:55:23 -0300

    Add files via upload

---
+++ mackenzie_2.py
@@ -0,0 +1,60 @@
+import os
+
+os.system('cls')
+
+class Coordenadas:
+    def __init__(self):
+        coord_origin_x,coord_origin_y = [int(x) for x in input('Coordenada de origem: ').split()] 
+
+        #define as coordenadas x,y
+        self.cox = coord_origin_x
+        self.coy = coord_origin_y
+
+        #Vai guardar as coordenadas e seus quadrantes correspondentes.
+        resultados = []
+
+        #armazena os resultados euclidianos e sua coordenadas
+        euclidianos = {}
+
+        n = int(input('\nQuantas coordenadas serão inseridas? '))
+
+        #Vai definir o quadrantes das N coordenadas que foram inseridas.
+        for i in range(n):
+            x, y = [int(x) for x in input().split()]
+
+            if x == self.cox or y == self.coy:
+                result = 'Coordenada ({},{}): Linha de Origem'.format(x,y)
+            elif x > 0 and y > 0:
+                result = 'Coordenada ({},{}): Primeiro Quadrante'.format(x,y)
+            elif x < 0 and y > 0:
+                result = 'Coordenada ({},{}): Segundo Quadrante'.format(x,y)
+            elif x < 0 and y < 0: 
+                result = 'Coordenada ({},{}): Terceiro Quadrante'.format(x,y)
+            else: 
+                result = 'Coordenada ({},{}): Quarto Quadrante'.format(x,y)
+            
+            #pitagoras
+            euclid = float(((((x - self.cox)**2) + ((y - self.coy)**2)) **0.5))
+
+            resultados.append(result)
+            euclidianos[str('({},{})'.format(x,y))] = euclid
+
+        #Armazena os maiores e menores valores, junto com sua chave, que é sua própria coordenada
+        min_key = min(euclidianos, key=euclidianos.get)
+        min_value = min(euclidianos.values())
+
+        max_value = max(euclidianos.values())
+        max_key = max(euclidianos, key=euclidianos.get)
+
+        os.system('cls')
+
+        print('Coordenada de origem({},{})\n'.format(self.cox,self.coy))
+
+        for r in resultados:
+            print('{}'.format(r))
+
+        print(f"Ponto mais próximo: {min_key}, distancia: {min_value:.2f}")
+        print(f"Ponto mais distante: {max_key}, distancia: {max_value:.2f}")
+    
+
+Coordenadas()
